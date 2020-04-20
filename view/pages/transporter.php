1816bc1b5b1742fb508ff82f50ae166aa6c65c38
Udith25 <ui25.isuranga@gmail.com>
2020-04-21 00:48:06 +0530

    adding file

---
+++ view/pages/transporter.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Transportor Page</title>
+</head>
+<body>
+
+	<main>
+		<div>
+			<h3>Update Bus Depature Time</h3>
+			
+		</div>	
+	</main>
+	
+</body>
+</html>
