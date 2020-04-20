9267b5c29ebddeb61afdd292863a090a7d3610c9
Udith25 <ui25.isuranga@gmail.com>
2020-04-21 00:46:59 +0530

    adding new table and file

--- view/pages/profile.php
+++ view/pages/profile.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<meta charset="UTF-8">
 	<title>My Profile</title>
 </head>
