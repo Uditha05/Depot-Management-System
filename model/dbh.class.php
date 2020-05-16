5f20cbc30686cdba32ebd07b5d4a902792c6be39
Udith25 <ui25.isuranga@gmail.com>
2020-05-16 22:13:43 +0530

    make previous version

--- model/dbh.class.php
+++ model/dbh.class.php
@@ -1,22 +1,23 @@
-<?php
-/**
- *
- */
-class Dbh
-{
+<?php 
 
-  private $servername = "localhost";
-  private $dbUsername = "root";
-  private $dbPassword = "";
-  private $dbName = "depot management system";
+class Dbh{
+	private $host = "localhost";
+	private $user = "root";
+	private $pwd = "";
+	private $dbName = "mainsystemdb";
 
-  protected function connect()
-  {
 
-    $dsn = 'mysql:host=' . $this->servername . ';dbname=' . $this->dbName;
-    $pdo = new PDO($dsn, $this->dbUsername, $this->dbPassword);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-    }
+	protected function connection(){
+		//set DSN
+		$dsn = 'mysql:host='.$this->host.';dbname='.$this->dbName;
+
+		//create pdo instance
+		$pdo = new PDO($dsn,$this->user,$this->pwd);
+
+		// set default fetch mode
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+		// return customise PDO object
+		return $pdo;
+	}
 }
-?>
